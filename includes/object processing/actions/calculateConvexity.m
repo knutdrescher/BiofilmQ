775c0a516bdb136ae1b1cdc9cfbaf0d85447150e
Eva Jimenez Siebert <eva.j.siebert@unibas.ch>
2023-10-04 18:08:20 +0200

    Changed convexity calculation: now calculated at pixel-precision

--- includes/object processing/actions/calculateConvexity.m
+++ includes/object processing/actions/calculateConvexity.m
@@ -10,24 +10,29 @@ end
 convexity = zeros(objects.NumObjects,1);
 
 N = objects.NumObjects;
-
 PixelIdxList = objects.PixelIdxList;
 ImageSize = objects.ImageSize;
 
+fields = fieldnames(objects);
+fields2Keep = {'Connectivity', 'NumObjects', 'ImageSize', 'PixelIdxList'};
+fields(ismember(fields, fields2Keep)) = [];
+CC = rmfield(objects, fields);
+CC.NumObjects = 1;
+CC.PixelIdxList = {};
+
 parfor i = 1:N
-    try
-        [y, x, z] = ind2sub(ImageSize, PixelIdxList{i});
-        if sum(sum(diff(z))) % 3D
-            [~, v] = convhull(x,y,z);
-        else % 2D
-            [~, v] = convhull(x,y);
+    try 
+        CC_temp = CC;
+        CC_temp.PixelIdxList = PixelIdxList(i);
+        if (length(ImageSize) == 3) && ((ImageSize(3))>1)
+            convexity(i) = table2array(regionprops3(CC_temp, 'Solidity')); 
+        else 
+            convexity(i) = regionprops(CC, 'Solidity');
         end
-        convexity(i) = numel(PixelIdxList{i})/v;
     catch
         convexity(i) = NaN;
     end
 end
-
 
 convexity = num2cell(convexity);
 [objects.stats.Shape_Convexity] = convexity{:};
